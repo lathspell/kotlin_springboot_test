80e18805357419b1bcee50e2bd8820811ac89f6a
Christian Brunotte <cb@lathspell.de>
2018-07-30 23:37:24 +0200

    tests

--- README.md
+++ README.md
@@ -26,3 +26,8 @@ Different HTTP Client libaries testet:
 * Spring "TestRestTemplate" was more or less ok
 * "Retrofit" was not as good; easy to forget .execute()
 * "Fuel" could not be convinced to serialize a class to JSON even with the "fuel-jackson" dependency
+
+Startup Time
+------------
+* original: Started KotlinSpringbootTestApplicationKt in 13.984 seconds (JVM running for 16.958)
+* ComponentScan with LazyInit: Started KotlinSpringbootTestApplicationKt in 13.355 seconds (JVM running for 14.348) 
--- src/main/kotlin/de/lathspell/test/KotlinSpringbootTestApplication.kt
+++ src/main/kotlin/de/lathspell/test/KotlinSpringbootTestApplication.kt
@@ -1,10 +1,22 @@
 package de.lathspell.test
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackageClasses = [Application::class], excludeName = ["*"])
+@ComponentScan(lazyInit = true, basePackages = ["de.lathspell.test"], includeFilters = [])
 class Application {
+
+    @Configuration
+    @Import(HttpMessageConvertersAutoConfiguration::class)
+    @ComponentScan(lazyInit = true, basePackages = ["de.lathspell.test"], includeFilters = [])
+    internal class LocalConfig
 
 }
 
